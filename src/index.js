05892e05ea4e4306e1001ebc9bee95b36cf5f981
jelee603 <jelee603@gmail.com>
2020-09-24 00:38:22 +0900

    Tic Tac Toe 게임

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Game from './components/Game';
 import * as serviceWorker from './serviceWorker';
+// import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Game />,
   document.getElementById('root')
 );
 
